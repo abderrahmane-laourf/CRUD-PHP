5dfef62443dab03c1dd737648705fed06edfd9c2
ABDERRAHMANE LAOURF <152749612+abderrahmane-laourf@users.noreply.github.com>
2025-07-01 21:02:24 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -7,7 +7,7 @@
         die("❌ ID non spécifié.");
     }
 
-    // recupiration des donnee
+    // recupiration des donne
     $stmt = $pdo -> prepare("SELECT * FROM users WHERE id = :id");
     $stmt->execute([':id'=>$id]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
