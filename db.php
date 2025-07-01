635ddf995a21de48844ea0991db386e9a2d58474
! ๛༢ <128975433+devddine@users.noreply.github.com>
2025-07-01 22:50:14 +0100

    Update db.php

--- db.php
+++ db.php
@@ -4,10 +4,9 @@
     $username = 'root';
     $password = 'root';
 
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     try{
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         die("error connexion".$e -> getMessage());
     }
