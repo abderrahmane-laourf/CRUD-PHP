41c153056fa224a32f38fa32e7f7de8bdfe1fea9
ABDERRAHMANE LAOURF <152749612+abderrahmane-laourf@users.noreply.github.com>
2025-07-02 23:36:13 +0100

    Update db.php

--- db.php
+++ db.php
@@ -4,6 +4,7 @@
     $username = 'root';
     $password = 'root';
 
+
     try{
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
